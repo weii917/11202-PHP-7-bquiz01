1c5a2f8af8f3cb4c105ff6dcd4624ffc18b242fa
wei <lin951016@gmail.com>
2023-12-04 15:20:13 +0800

    透過判斷檔案是否存在決定引入的頁面

--- index.php
+++ index.php
@@ -38,17 +38,25 @@
 			<?php // include "./front/main.php";
 			?>
 			<?php
+		
 			$do = $_GET['do'] ?? 'main';
-			switch ($do) {
-				case "login";
-					include "./front/login.php";
-					break;
-				case "news";
-					include "./front/news.php";
-					break;
-				default:
-					include "./front/main.php";
+			$file = "./front/{$do}.php";
+			if(file_exists($file)){
+				include $file;
+			}else{
+				include "./front/main.php";
 			}
+			// 原始
+			// switch ($do) {
+			// 	case "login";
+			// 		include "./front/login.php";
+			// 		break;
+			// 	case "news";
+			// 		include "./front/news.php";
+			// 		break;
+			// 	default:
+			// 		include "./front/main.php";
+			// }
 
 			?>
 			<div id="alt" style="position: absolute; width: 350px; min-height: 100px; word-break:break-all; text-align:justify;  background-color: rgb(255, 255, 204); top: 50px; left: 400px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
