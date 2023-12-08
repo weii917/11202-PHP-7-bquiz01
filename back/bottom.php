c16654389efbe9d74b33770df2f1be64843aab89
wei <lin951016@gmail.com>
2023-12-08 10:16:19 +0800

    複製title做bottom

--- back/bottom.php
+++ back/bottom.php
@@ -1,11 +1,13 @@
 <div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">進站總人數管理</p>
-    <form method="post" action="./api/edit_total.php">
+    <p class="t cent botli">頁尾版權資料管理</p>
+    <form method="post" action="/api/edit_bottom.php">
         <table style="width:50%;margin:auto">
             <tbody>
                 <tr class="yel">
-                    <td width="50%">進站總人數</td>
-                    <td width="50%"><input type="number" name="total" value="<?=$Total->find(1)['total'];?>"></td>                            
+                    <td width="50%">頁尾版權資料</td>
+                    <td width="50%">
+                        <input type="text" name="bottom" value="<?=$Bottom->find(1)['bottom'];?>">
+                    </td>
                 </tr>
             </tbody>
         </table>
