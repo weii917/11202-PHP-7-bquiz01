4ef28ac54476248160f0d722bdd259a23773814d
wei <lin951016@gmail.com>
2023-12-11 15:32:44 +0800

    建立選單管理頁面

--- api/db.php
+++ api/db.php
@@ -147,7 +147,7 @@ $Mvim=new DB('mvim');
 $Image=new DB('image');
 $News=new DB('news');
 $Admin=new DB('admin');
-
+$Menu=new DB('menu');
 if(isset($_GET['do'])){
     $DB=${ucfirst($_GET['do'])};
 }else{
