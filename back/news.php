a8b103803bd42923886a3dd4c23ac02b789f7c55
wei <lin951016@gmail.com>
2023-12-11 13:16:43 +0800

    完成上一頁下一頁

--- back/news.php
+++ back/news.php
@@ -3,47 +3,54 @@
     <form method="post" action="./api/edit.php">
         <table width="100%" style="text-align: center;">
             <tbody>
-                <tr class="yel">               
+                <tr class="yel">
                     <td width="80%">最新消息資料</td>
                     <td width="10%">顯示</td>
-                    <td width="10%">刪除</td>   
+                    <td width="10%">刪除</td>
                 </tr>
                 <?php
-                $total=$DB->count();
-                $div=5;
-                $pages=ceil($total/$div);
-                $now=$_GET['p']??1;
-                $start=($now-1)*$div;
-                $rows=$DB->all(" limit $start,$div");
-                foreach ($rows as $row){               
+                $total = $DB->count();
+                $div = 5;
+                $pages = ceil($total / $div);
+                $now = $_GET['p'] ?? 1;
+                $start = ($now - 1) * $div;
+                $rows = $DB->all(" limit $start,$div");
+                foreach ($rows as $row) {
                 ?>
-                <tr>
-                   
-                    <td><textarea type="text" name="text[<?=$row['id'];?>]" style="width:90%;height:60px"><?=$row['text'];?></textarea></td>
-                    <td><input type="checkbox" name="sh[]" value="<?=$row['id'];?>"<?=($row['sh']==1)?'checked':'';?>></td>
-                    <td><input type="checkbox" name="del[]" value="<?=$row['id'];?>"></td>
-                  
-                </tr>
+                    <tr>
+
+                        <td><textarea type="text" name="text[<?= $row['id']; ?>]" style="width:90%;height:60px"><?= $row['text']; ?></textarea></td>
+                        <td><input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                        <td><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
+
+                    </tr>
                 <?php
-                 }
+                }
                 ?>
             </tbody>
         </table>
         <!-- 判斷當前頁等於字型變大 -->
         <div class="cent">
             <?php
-            for($i=1;$i<=$pages;$i++){
-                $fontsize=($now==$i)?'24px':'16px';
+            if ($now > 1) {
+                $prev=$now-1;
+                echo "<a href='?do=news&p=$prev'> < </a>"; 
+            }
+            for ($i = 1; $i <= $pages; $i++) {
+                $fontsize = ($now == $i) ? '24px' : '16px';
                 echo "<a href='?do=news&p=$i' style='font-size:$fontsize'> $i </a>";
             }
-            
+            if ($now < $pages) {
+                $next=$now+1;
+                echo "<a href='?do=news&p=$next'> > </a>"; 
+            }
             ?>
         </div>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增最新消息資料"></td>
+                    <input type="hidden" name="table" value="<?= $do; ?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增最新消息資料"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
