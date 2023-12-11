e7924f3543aaaf18e0bc7021f5f98323474b5d15
wei <lin951016@gmail.com>
2023-12-11 13:30:47 +0800

    製作校園映像分頁

--- back/image.php
+++ back/image.php
@@ -10,29 +10,51 @@
                     <td></td>
                 </tr>
                 <?php
-                $rows=$DB->all();
-                foreach ($rows as $row){
+                $total = $DB->count();
+                $div = 3;
+                $pages = ceil($total / $div);
+                $now = $_GET['p'] ?? 1;
+                $start = ($now - 1) * $div;
+                $rows = $DB->all(" limit $start,$div");
 
-               
+                foreach ($rows as $row) {
+
+
                 ?>
-                <!-- 做mvim時發現沒有id取用，所以這裡藏一個id讓mvim有id依據刪除編輯 -->
-                <tr>
-                    <td width="45%"><img src="./img/<?=$row['img'];?>"style="width:150px;height:100px" alt=""></td>
-                    <input type="hidden" name="id[]" value="<?=$row['id'];?>">
-                    <td width="7%"><input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=($row['sh']==1)?'checked':'';?>></td>
-                    <td width="7%"><input type="checkbox" name="del[]" value="<?=$row['id'];?>"></td>
-                    <td><input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?=$do;?>&id=<?=$row['id'];?>')" value="更新校園映像"></td>
-                </tr>
+                    <!-- 做mvim時發現沒有id取用，所以這裡藏一個id讓mvim有id依據刪除編輯 -->
+                    <tr>
+                        <td width="45%"><img src="./img/<?= $row['img']; ?>" style="width:100px;height:68px" alt=""></td>
+                        <input type="hidden" name="id[]" value="<?= $row['id']; ?>">
+                        <td width="7%"><input type="checkbox" name="sh[]" value="<?= $row['id']; ?>" <?= ($row['sh'] == 1) ? 'checked' : ''; ?>></td>
+                        <td width="7%"><input type="checkbox" name="del[]" value="<?= $row['id']; ?>"></td>
+                        <td><input type="button" onclick="op('#cover','#cvr','./modal/upload.php?table=<?= $do; ?>&id=<?= $row['id']; ?>')" value="更新校園映像"></td>
+                    </tr>
                 <?php
-                 }
+                }
                 ?>
             </tbody>
         </table>
+        <div class="cent">
+            <?php
+            if ($now > 1) {
+                $prev = $now - 1;
+                echo "<a href='?do=$do&p=$prev'> < </a>";
+            }
+            for ($i = 1; $i <= $pages; $i++) {
+                $fontsize = ($now == $i) ? '24px' : '16px';
+                echo "<a href='?do=$do&p=$i' style='font-size:$fontsize'> $i </a>";
+            }
+            if ($now < $pages) {
+                $next = $now + 1;
+                echo "<a href='?do=$do&p=$next'> > </a>";
+            }
+            ?>
+        </div>
         <table style="margin-top:40px; width:70%;">
             <tbody>
                 <tr>
-                    <input type="hidden" name="table" value="<?=$do;?>">
-                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?=$do;?>.php?table=<?=$do;?>')" value="新增校園映像圖片"></td>
+                    <input type="hidden" name="table" value="<?= $do; ?>">
+                    <td width="200px"><input type="button" onclick="op('#cover','#cvr','./modal/<?= $do; ?>.php?table=<?= $do; ?>')" value="新增校園映像圖片"></td>
                     <td class="cent"><input type="submit" value="修改確定"><input type="reset" value="重置"></td>
                 </tr>
             </tbody>
