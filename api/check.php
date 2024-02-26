bd2a9a47208bc9d7d9770d88f8b87761d214b2f2
wei <lin951016@gmail.com>
2024-02-26 08:49:53 +0800

    新增過濾表單資料功能

--- api/check.php
+++ api/check.php
@@ -1,8 +1,12 @@
 <?php include_once "db.php";
+// 過濾表單資料
+$acc=htmlspecialchars($_POST['acc']);
+$pw=htmlspecialchars($_POST['pw']);
+
 
 // 帳號密碼檢查
-if($Admin->count(['acc'=>$_POST['acc'],'pw'=>$_POST['pw']])>0){
-    $_SESSION['login']=$_POST['acc'];
+if($Admin->count(['acc'=>$acc,'pw'=>$pw])>0){
+    $_SESSION['login']=$acc;
     to("../back.php");
 }else{
     to("../index.php?do=login&error=帳號密碼錯誤");
